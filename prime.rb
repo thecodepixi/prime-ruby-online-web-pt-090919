67b1cd1f9d0cfbe61edec65b9a93cec42f6c4c47
Emily Harber <emily.a.harber@gmail.com>
2019-09-17 01:22:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,4 @@
 # Add  code here!
 def prime?(n)
-  i = 5 
-  if n <= 3 
-    return true 
-  elsif n % 2 == 0 || n % 3 == 0 || n < 1 
-    return false 
-  else 
-    while i * i <= n 
-      if n % i == 0 || n % (i + 2) == 0 
-        return false 
-      end 
-    i += 6 
-    end 
-  end 
   
 end 
