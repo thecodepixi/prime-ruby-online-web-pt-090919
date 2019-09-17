fa55bd5ee70c492aa3f3fb68cee052e8bd11da3f
Emily Harber <emily.a.harber@gmail.com>
2019-09-17 01:20:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(n)
   i = 5 
   if n <= 3 
     return true 
-  elsif n % 2 == 0 || n % 3 == 0 
+  elsif n % 2 == 0 || n % 3 == 0 || n < 1 
     return false 
   else 
     while i * i <= n 
