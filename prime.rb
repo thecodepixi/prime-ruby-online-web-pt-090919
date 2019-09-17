49dbb194a0540399bca6aabdb8d76828e8610729
Emily Harber <emily.a.harber@gmail.com>
2019-09-17 01:34:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,11 +11,10 @@ def prime?(n)
     while i * i <= n 
       if n % i == 0 || n % (i+2) == 0 
         return false 
-        i += 6 
-      else 
-        return true 
       end 
+      i += 6 
     end 
+    return true 
   end 
   
 end 
