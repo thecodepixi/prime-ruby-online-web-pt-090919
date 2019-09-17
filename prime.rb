d640b45821d17af0c20460ae39e406e30d145f03
Emily Harber <emily.a.harber@gmail.com>
2019-09-17 01:39:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+
+def prime?(n)
+  i = 5 
+  if n <= 1 
+    return false 
+  elsif n <= 3 
+    return true 
+  elsif n % 2 == 0 || n % 3 == 0 
+    return false 
+  else 
+    while i * i <= n 
+      if n % i == 0 || n % (i+2) == 0 
+        return false 
+      end 
+      i += 6 
+    end 
+    return true 
+  end 
+  
+end 
